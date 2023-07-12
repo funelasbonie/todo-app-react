2f1fe3a055535dfbc67eb0592aaa6892bab7fd2f
Bonie Funelas <funelasbonie@gmail.com>
2023-07-12 11:00:39 +0800

    Add comments in todo items

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -7,8 +7,24 @@ export default class Todo extends React.Component {
         this.state = {
             todoList: [],
             newTodo: '',
+            commentText: '',
+            editingCommentId: null,
         };
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     handleInputChange = (event) => {
         const { value } = event.target;
@@ -36,9 +52,10 @@ export default class Todo extends React.Component {
         const newTodos = [
             ...this.state.todoList,
             {
-                id: Date.now(),
+                id: Date.now(), //make it guid
                 text: this.state.newTodo,
                 isEditing: false,
+                comments: [],
             },
         ];
 
@@ -46,7 +63,6 @@ export default class Todo extends React.Component {
     }
 
     toggleUpdate = (id) => {
-        console.log("updatedTodos: ", updatedTodos)
         const updatedTodos = this.state.todoList.map((todo) => {
             if (todo.id === id) {
                 return {
@@ -77,8 +93,128 @@ export default class Todo extends React.Component {
         this.setState({ todoList: updatedTodos });
     }
 
+
+
+
+
+
+
+
+
+    handleCommentInputChange = (e) => {
+        this.setState({ commentText: e.target.value });
+    }
+
+    handleUpdateCommentInputChange = (todoId, commentId, e) => {
+        const updatedTodos = this.state.todoList.map((todo) => {
+            if (todo.id === todoId) {
+                const updatedComments = todo.comments.map((comment) => {
+                    if (comment.id === commentId) {
+                        return {
+                            ...comment,
+                            text: e.target.value,
+                        };
+                    }
+                    return comment
+                })
+                console.log("updatedComments: ", updatedComments)
+                return {
+                    ...todo,
+                    comments: updatedComments
+                };
+            }
+            return todo;
+        });
+        this.setState({ todoList: updatedTodos });
+    }
+
+    addComment = (id) => {
+        const { todoList, commentText } = this.state;
+        const updatedTodos = todoList.map((todo) => {
+            if (todo.id === id) {
+                const newComment = {
+                    id: Date.now(),
+                    text: commentText,
+                };
+                return {
+                    ...todo,
+                    comments: [...todo.comments, newComment],
+                };
+            }
+            return todo;
+        });
+
+        this.setState({ todoList: updatedTodos, commentText: '' });
+    }
+
+    removeComment = (todoId, commentId) => {
+        const updatedTodos = this.state.todoList.map((todo) => {
+            if (todo.id === todoId) {
+                const updatedComments = todo.comments.filter(
+                    (comment) => comment.id !== commentId
+                );
+                return {
+                    ...todo,
+                    comments: updatedComments,
+                };
+            }
+            return todo;
+        });
+
+        this.setState({ todoList: updatedTodos });
+    }
+
+    toggleUpdateComment = (commentId) => {
+        this.setState({ editingCommentId: commentId });
+    }
+
+    saveUpdatedComment = (todoId, commentId) => {
+        const updatedTodos = this.state.todoList.map((todo) => {
+            if (todo.id === todoId) {
+                const updatedComments = todo.comments.map((comment) => {
+                    if (comment.id === commentId) {
+                        return {
+                            ...comment,
+                            text: this.state.commentText,
+                        };
+                    }
+                    return comment;
+                });
+                return {
+                    ...todo,
+                    comments: updatedComments,
+                };
+            }
+            return todo;
+        });
+
+        this.setState({
+            todos: updatedTodos,
+            commentText: '',
+            editingCommentId: null,
+        });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     render() {
-        const { todoList, newTodo } = this.state;
+        const { todoList, newTodo, commentText, editingCommentId } = this.state;
 
         return (
             <div>
@@ -93,21 +229,51 @@ export default class Todo extends React.Component {
                     <button onClick={this.setTodo}>Add</button>
                 </div>
                 <ul>
-                    {todoList.map((todo) => (
-                        <li key={todo.id}>
-                            {
-                                todo.isEditing
-                                    ? (<input type="text" value={todo.text} onChange={(e) => this.handleUpdateInputChange(todo.id, e)} />)
-                                    : (<span>{todo.text}</span>)
-                            }
-                            {
-                                todo.isEditing
-                                    ? (<button onClick={() => this.saveUpdatedTodo(todo.id)}>Save</button>)
-                                    : (<button onClick={() => this.toggleUpdate(todo.id)}>Edit</button>)
-                            }
-                            <button onClick={() => this.removeTodo(todo.id)}>Delete</button>
-                        </li>
-                    ))}
+                    {
+                        todoList.map((todo) => (
+                            <li key={todo.id}>
+                                {
+                                    todo.isEditing
+                                        ? (<input type="text" value={todo.text} onChange={(e) => this.handleUpdateInputChange(todo.id, e)} />)
+                                        : (<span>{todo.text}</span>)
+                                }
+                                {
+                                    todo.isEditing
+                                        ? (<button onClick={() => this.saveUpdatedTodo(todo.id)}>Save</button>)
+                                        : (<button onClick={() => this.toggleUpdate(todo.id)}>Edit</button>)
+                                }
+                                <button onClick={() => this.removeTodo(todo.id)}>Delete</button>
+                                <div>
+                                    <input
+                                        type="text"
+                                        placeholder="Enter a comment"
+                                        value={commentText}
+                                        onChange={this.handleCommentInputChange}
+                                    />
+                                    <button onClick={() => this.addComment(todo.id)}>Add Comment</button>
+                                    <ul>
+                                        {
+                                            todo.comments.map((comment) => (
+                                                <li key={comment.id}>
+                                                    {
+                                                        editingCommentId === comment.id
+                                                            ? (<input type="text" value={comment.text} onChange={(e) => this.handleUpdateCommentInputChange(todo.id, comment.id, e)} />)
+                                                            : (comment.text)
+                                                    }
+                                                    {
+                                                        editingCommentId === comment.id
+                                                            ? (<button onClick={() => this.saveUpdatedComment(todo.id, comment.id)}>Save Comment</button>)
+                                                            : (<button onClick={() => this.toggleUpdateComment(comment.id)}>Edit Comment</button>)
+                                                    }
+                                                    <button onClick={() => this.removeComment(todo.id, comment.id)}>Delete Comment</button>
+                                                </li>
+                                            ))
+                                        }
+                                    </ul>
+                                </div>
+                            </li>
+                        ))
+                    }
                 </ul>
             </div>
         )
